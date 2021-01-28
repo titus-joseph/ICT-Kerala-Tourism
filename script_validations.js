d1a6c68a8b2c45c5af14b7838308c06001e4beed
titusjoseph <titusjoseph07@gmail.com>
2021-01-28 14:30:25 +0000

    Fix

--- script_validations.js
+++ script_validations.js
@@ -1,97 +1,70 @@
 function signinvalidation(){
-  var username=  document.getElementById("inputEmail3");
-  document.write("Hello "+ username.value  + " Kerala tourisam website , you have successfully logged in");
-  return true;
+      var username=  document.getElementById("inputEmail3");
+      document.write("Hello "+ username.value  + " welcome to Kerala tourisam website , you have successfully logged in");
+      return true;
 
-}
+    }
 
 // function to validate the email in signup page;
 
 
-function signinValidation(){
-var email=document.getElementById("inputEmail4");
-var errorlabel=document.getElementById("errormsg");
+function signinValidation()
+{
+     var email=document.getElementById("inputEmail4");
+     var errorlabel=document.getElementById("errormsg");
    // console.log(email.value);
   let regemail=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
-  if(regemail.test(email.value)){
+  if(regemail.test(email.value))
+    {
       //errorlabel.innerHTML="Valid";
+      errorlabel.textContent="valid email";
       errorlabel.style.color="green";  
       return true;    
 
      }
   else{
-      errorlabel.innerHTML="Invalid email id";
+      errorlabel.textContent="invalid email ";
       errorlabel.style.color="red";  
       return false;
-     }
-}
-  function phoneNumbervalidation(){
-    var phonenumber=document.getElementById("phonenumber");
-    var errorlabelphone=document.getElementById("errormsgphone");
-    console.log(phonenumber.value);
-     var regphone=/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
-    if(regphone.test(phonenumber.value)){
-      //errorlabelphone.innerHTML="Valid phone number";
-      errorlabelphone.style.color="green"; 
-      return true; 
-    }
-    else {
-      errorlabelphone.innerHTML="invalid phone number";
-      errorlabelphone.style.color="red"; 
-      return false; 
-    }
+      }
+ }
+
+   function phoneNumbervalidation()
+   {
+       var phonenumber=document.getElementById("phonenumber");
+       var errorlabelphone=document.getElementById("errormsgphone");
+       var regphone=/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+      if(regphone.test(phonenumber.value))
+      {
+          errorlabelphone.textContent="valid phone number";
+          errorlabelphone.style.color="green"; 
+          return true; 
+         }
+     else 
+     {
+         errorlabelphone.textContent="invalid phone number";
+         errorlabelphone.style.color="red"; 
+        
+         return false; 
+      }
   }
 
-   function validatePassword(){
-    //  console.log("inside password fun")
-    var errorpwd=document.getElementById("errormsgpwd")
-    var pwd=document.getElementById("inputPassword4");
-    //var pwdl=pwd.value.lenghth;
+
+   function validatePassword()
+   {
    
-    //console.log(pwd.value)
-   // console.log(pwd.value.length)
-    if(pwd.value.length<=8)
-       {
-      errorpwd.innerHTML="Password must be minimum of 8 charecter";
-      errorpwd.style.color="red";
-      return false;
-       }
-       else if(pwd.value.search(/[0-9]/)==-1)
-       {
-        errorpwd.innerHTML="Password must contain a numeric value";
-        errorpwd.style.color="red";
-        return false;
-       }
-    // else 
-    //    {
-    //     errorpwd.innerHTML="length of the password is greater than 8";
-    //     errorpwd.style.color="red";
-    //   return true;
-    //    }
-
+    var errorpwd=document.getElementById("errormsgpwd");
+    var pwd=document.getElementById("inputPassword4");
+     if(pwd.value.length<=8)
+        {
+           errorpwd.innerHTML="Password must be minimum of 8 charecter";
+           errorpwd.style.color="red";
+           return false;
+         }
+      else{
+        
+          return true;
+          
+          }
+     
   }
- 
-
-
-
-
-//   // password validation functiom 
-//   var pwd=document.getElementById("inputPassword4");
-// //   function validatePassword(){
-
-
-// // pwd
-// //   }
-// document.getElementById("signinbutton").addEventListener("click", validatepassword);
-
-//    function validatepassword()
-//    {
-//     var lowerCaseLetters = /[a-z]/g;
-//     if(pwd.value.match(lowerCaseLetters)) 
-//     {
-//       console.log("contain lowercase");                       
-//     }
-//                       else{
-//                               console.log("no lowercase");
-//                              }
-//   }
